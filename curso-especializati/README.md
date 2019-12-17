247add309e0d5075961136dfd0fc8a39adeddb84
leandroaa7 <leandroaalexandre@gmail.com>
2019-12-17 11:58:23 -0300

    estudo de rotas e comentários no README.MD

--- curso-especializati/README.md
+++ curso-especializati/README.md
@@ -7,6 +7,10 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
 </p>
 
+## Sobre o Projeto Laravel
+
+Projeto utilizando o conteúdo do curso de laravel 6.x do canal do youtube EspecializaTi do [link](https://www.youtube.com/playlist?list=PLVSNL1PHDWvQBtcH_4VR82Dg-aFiVOZBY)
+
 ## About Laravel
 
 Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
--- curso-especializati/routes/web.php
+++ curso-especializati/routes/web.php
@@ -11,6 +11,213 @@
 |
 */
 
+/* Para utilizar as rotas não precisa importar nada
+ * porém estou importando apenas para a IDE não acusar algum erro 
+ * para endender melhor procure sobre PSR e autoload
+ * */
+
+/**Importação errada das rotas
+ * use Illuminate\Routing\Route;
+ */
+
+/**importação correta */
+
+use Illuminate\Support\Facades\Route;
+
+/** Referência do conteúdo está 
+ * no curso https://www.youtube.com/playlist?list=PLVSNL1PHDWvQBtcH_4VR82Dg-aFiVOZBY)
+ * e no link https://laravel.com/docs/master/routing */
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/teste", function () {
+    return "rota teste";
+});
+
+Route::get('/view-teste', function () {
+    return view("teste");
+});
+
+Route::get('/empresa', function () {
+    return view('site.contact');
+});
+
+Route::get('/contato', function () {
+    return 'contato';
+});
+
+/**
+ * Rota do tipo POST
+ */
+Route::post("/register", function () {
+    return '';
+});
+
+/** Rota do tipo ANY, ela recebe todos os tipos de requisições HTTP e trata da mesma forma */
+Route::any('/any', function () {
+    return 'any';
+});
+
+/** Rota do tipo MATCH, similar a rota do tipo any, porém é necessário definir quais métodos HTTP são permitidos */
+Route::match(['post', 'put', 'get'], '/match', function () {
+    return 'match';
+});
+
+/**AULA 11 - Rotas com parâmetros no Laravel*/
+
+/**Rota do tipo GET com um parámetro chamado flag
+ * Para utilizar o valor do parámetro adicione um atributo a função de callback 
+ * Caso seja apenas um atributo a variável não precisa ter o mesmo nome do parámetro */
+Route::get('/categorias/{flag}', function ($prim) {
+    return "Produtos da categoria: {$prim}";
+});
+
+Route::get('/categoria/{flag}/posts', function ($flag) {
+    return "Posts da categoria: {$flag}";
+});
+
+/**Parâmetros opcionais */
+/** Rota do tipo GET com um parámetro chamado idProduct, o ? indica que é um parámetro opcional
+ * é preciso definir o valor padrão no callback
+ */
+Route::get('/produtos/{idProduct?}', function ($idProduct = "valor default") {
+    return "produto(s) {$idProduct}";
+});
+
+/** AULA 12 - Rotas no Laravel - redirect e view */
+/**Para redirecionar para uma rota utilize um HELPER do laravel chamado REDIRECT
+ * HELPERS são funções que estão disponíveis em todo o código
+ * https://laravel.com/docs/master/helpers*/
+
+/** REDIRECT */
+Route::get('/redirect1', function () {
+    return redirect('/redirect2');
+});
+
+Route::get('/redirect2', function () {
+    return 'redirect2';
+});
+
+/** Rota /redirect3 que redireciona para /redirect2 */
+Route::redirect('/redirect3', '/redirect2');
+
+/** VIEW */
+/**Rota /view que não possui lógica, ela apenas renderiza a página view */
+Route::view('/view', 'welcome');
+
+/**Rota view que renderiza página que está na pasta site no diretório /resources/views/site/ */
+Route::view('/view/contact', 'site.contact');
+
+/** Rota view com o parámetro ID com valor padrão de teste  */
+Route::view('/view/contact/id', 'site.contact', ['id' => 'teste']);
+
+/** AULA 13 - Rotas nomeadas no Laravel */
+/**Rota com nome url.name */
+Route::get('/nome-url', function () {
+    return 'hey hey hey';
+})->name('url.name');
+
+/** rota que redireciona pelo nome da rota */
+Route::get('/redirect4', function () {
+    return  redirect()->route('url.name');
+});
+
+/** AULA 14 - Grupo de Rotas no Laravel */
+
+/** Rota autenticada  */
+/** rota que utiliza o middleware auth, quem acessar /auth deve estar autenticado
+ * caso contrário será redirecionado para o /login  
+ * está rota retornará um erro enquanto não existir a rota COM O NOME login
+ */
+Route::get('/auth', function () {
+    return 'rota autenticada';
+})->middleware(['auth']);
+
+Route::get('/login', function () {
+    return 'login';
+})->name('login');
+
+/**Grupo de rotas */
+
+/**GRUPO DE ROTAS 1 - Define individualmente MIDDLEWARE, PREFIX, NAMESPACE E NAME */
+
+/**Rota MIDDLEWARE com um grupo de rotas, em todas as rotas que estão dentro do grupo abaixo será aplicado o middleware ou o conjunto de middlewares  */
+Route::middleware([])->group(function () {
+
+    /**Rota PREFIX com um grupo de rotas, em todas as rotas que estão dentro do grupo abaixo será aplicado o prefixo /admin
+     * ou seja
+     * para acessar a /dashboard será necessário acessar a url /admin/dashboard
+     */
+    Route::prefix('admin')->group(function () {
+
+        /**Rota NAMESPACE com um grupo de rotas, 
+         * todos os controllers utilizados nas funções deste grupo estão em uma pasta com o nome definido pelo namespace
+         * Neste caso é o Admin, então ao invés de definir o controller de uma rota com Admin\TesteController@teste basta adicionar o TesteController*/
+        Route::namespace('Admin')->group(function () {
+
+            /**Rota NAME com um grupo de rotas
+             * todas as rotas podem possuir um nome, o NAME define um prefixo antes do nome
+             * neste caso seria admin, Route::name('admin.') . 
+             * logo qualquer nome de rota como por exemplo, Route::get('/', 'TesteController@teste')->name('home'); poderá ser acessado por admin.home
+             */
+            Route::name('admin.')->group(function () {
+                Route::get('/', 'TesteController@teste')->name('home');
+                Route::get('/dashboard', 'TesteController@teste');
+                Route::get('/financeiro', 'TesteController@teste');
+                Route::get('/produtos', 'TesteController@teste');
+                Route::get('/redirect', function () {
+                    return redirect()->route('admin.home');
+                });
+            });
+        });
+
+        /* Rota utilizando um controller
+         * Ficar atento com a pasta onde o diretório está, se está em um pasta, exemplo Admin, deve colocar antes do controler o Admin\
+         * Controller criado no diretório app/Http/Controllers/Admin
+         * Para criar o controller foi utilizado o comando php artisan make:controller Admin/TesteControlle
+         */
+        Route::get('/teste', 'Admin\TesteController@teste');
+        /**
+         * não é recomendado retornar uma função de callback
+         * defina as lógicas nos controllers 
+         * diretório /app/Http/Controllers/NomeController
+         * Route::get('user', 'NomeController@nomeMetodo');
+         * Se houver algum sub diretório adicionar antes do controler ou um grupo de rotas
+         * Exemplo de rota com sub diretório Route::get('user', 'NomeDoDiretorioCasoExista\NomeController@nomeMetodo');
+         */
+    });
+});
+
+/**GRUPO DE ROTAS 2 - Parâmetros middleware,prefix,name e namespace são definidos em um Route::group apenas 
+ * para definir o name é necessário utilizar a chave AS dentro do array do group
+ */
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin2',
+    'namespace' => 'Admin',
+    'as' => 'admin.'
+], function () {
+    Route::get('/', 'TesteController@teste')->name('home');
+    Route::get('/dashboard', 'TesteController@teste');
+    Route::get('/financeiro', 'TesteController@teste');
+    Route::get('/produtos', 'TesteController@teste');
+    Route::get('/redirect', function () {
+        return redirect()->route('admin.home');
+    });
+});
+
+/**Rota middleware com um grupo de rotas que utilizam o middleware AUTH */
+Route::middleware(['auth'])->group(function () {
+    Route::get('/secret/', function () {
+        return 'secret admin';
+    });
+});
+
+/**AULA 15 - Comandos do Artisan para Rotas no Laravel  */
+
+/** php artisan route:list 
+ * lista todos as rotas com detalhes
+*/
+
